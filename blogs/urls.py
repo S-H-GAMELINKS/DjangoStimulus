f030a5068d89f2302dfb242a4cd67c74ca04d3a2
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-10 17:07:03 +0900

    render index.html

--- blogs/urls.py
+++ blogs/urls.py
@@ -6,6 +6,7 @@ router = routers.DefaultRouter()
 router.register(r'posts', views.PostViewSet)
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
