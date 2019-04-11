062d5db6337d154536c3c645e54400ff14f147ee
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-11 11:55:27 +0900

    show post

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,6 +7,7 @@ router.register(r'posts', views.PostViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('posts/<int:post_id>/', views.show, name='show'),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import Http404
 from .models import Post
 from rest_framework import viewsets
 from .serializers import PostSerializer
@@ -11,3 +11,11 @@ class PostViewSet(viewsets.ModelViewSet):
 def index(request):
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts': posts})
+
+def show(request, post_id):
+    try:
+        post = Post.objects.get(pk=post_id)
+    except Post.DoesNotExit:
+        raise Http404("Post does not exist!")
+
+    return render(request, 'show.html', {'post': post})
